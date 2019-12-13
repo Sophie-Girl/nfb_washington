8f4e20d8f13c4e004bdf74e08f5aee3de45e6038
sconnell <sconnell@nfb.org>
2019-12-13 15:14:57 -0500

    time element issues

--- src/form_factory/date_elements.php
+++ src/form_factory/date_elements.php
@@ -61,7 +61,7 @@ class date_elements extends select_elements
     {
         $this->element_id = 'meeting_time'; $this->type = 'datetime';
         $this->size = '20'; $this->date_date_element = 'none';
-        $this->date_time_element = 'time'; $this->date_time_format = 'H:i';
+        $this->date_time_element = 'time'; $this->date_time_format = 'h:i';
         $this->default_value = '05:00'; $this->build_time_elements($form);
     }
 
