65b2a7d6c2c004992b782a2cf57fc19fdf32b372
sconnell <sconnell@nfb.org>
2019-12-14 06:50:36 -0500

    update meeting form

--- src/form_factory/form_factory.php
+++ src/form_factory/form_factory.php
@@ -11,5 +11,14 @@ class form_factory extends markup_elements
         $this->meeting_time_element($form);
         $this->meeting_comments_element($form);
     }
+    public function build_update_mmeting_form(&$form)
+    {
+        $this->contact_first_name_element($form);
+        $this->contact_last_name_element($form);
+        $this->contact_email_element($form);
+        $this->meeting_day_element($form);
+        $this->meeting_time_element($form);
+        $this->meeting_comments_element($form);
+    }
     
 }
