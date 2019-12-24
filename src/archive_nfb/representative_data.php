2ee27d5a08b3b4910ce9559154f2027de76d60ae
sconnell <sconnell@nfb.org>
2019-12-24 14:20:58 -0500

    email class

--- src/archive_nfb/representative_data.php
+++ src/archive_nfb/representative_data.php
@@ -14,22 +14,22 @@ class representative_data extends query_base
         $this->rep_result = array(
             '0' => array("first_name" => "Test", "last_name" => "Rep", "rep_id" => "66612",
                 'district_number' => '1', 'state' => 'MD', 'meeting_id' => "", "meeting_date" => "",
-                'meeting_time = "", "meeting_location' => ''),
+                'meeting_time' => "", 'meeting_location' => ''),
             '1' => array("first_name" => "Some", "last_name" => "Jerk", "rep_id" => "66613",
                 'district_number' => '2', 'state' => 'MD', 'meeting_id' => "1", "meeting_date" => "2/10/2020",
-                'meeting_time = "4:30pm", "meeting_location' => 'Office Building'),
+                'meeting_time' => "4:30pm", 'meeting_location' => 'Office Building'),
             '2' => array("first_name" => "Another", "last_name" => "Jerk", "rep_id" => "66614",
                 'district_number' => '1', "state" => "CA", 'meeting_id' => "2", "meeting_date" => "2/10/2020",
-                'meeting_time = "6:00pm", "meeting_location' => 'Office Building'),
+                'meeting_time' => "6:00pm", 'meeting_location' => 'Office Building'),
             '3' => array('first_name' => 'Joe', "last_name" => "Smith", 'rep_id' => '66615',
                 'district_number' => '3', 'state' => 'MD', 'meeting_id' => "", "meeting_date" => "",
-                'meeting_time = "", "meeting_location' => ''),
+                'meeting_time' => "", 'meeting_location' => ''),
             '4' => array('first_name' => 'Jane', "last_name" => 'Dane', "rep_id" => '66616',
                 'district_number' => '4', 'state' => 'MD', 'meeting_id' => "", "meeting_date" => "",
-                'meeting_time = "", "meeting_location' => ''),
+                'meeting_time' =>"", 'meeting_location' => ''),
             '5' => array('first_name' => 'Sophie', "last_name" => 'Test', "rep_id" => '66617',
                 'district_number' => '5', 'state' => 'MD', 'meeting_id' => "3",
-                "meeting_date" => "2/10/2020",  'meeting_time = "5:30pm", "meeting_location' => 'Office Building'),
+                "meeting_date" => "2/10/2020",  'meeting_time' => "5:30pm", 'meeting_location' => 'Office Building'),
         );
     }
 
@@ -74,7 +74,7 @@ class representative_data extends query_base
          if($rep['meeting_id'] == "")
          {$meeting_status = "Not Scheduled";}
          else {$meeting_status = "Scheduled";}
-         $markup = $markup."<tr><th>".$rep['first_name']." ".$rep['last_name']."</th><th>".$rep['state']."</th><th>".$rep['district_number']."</th><th>".$meeting_status."</th><th>".$rep['Meeting_date']." ".$rep['meeting_time']."</th><th>".$rep['meeting_location']."</th></tr>";
+         $markup = $markup."<tr><th>".$rep['first_name']." ".$rep['last_name']."</th><th>".$rep['state']."</th><th>".$rep['district_number']."</th><th>".$meeting_status."</th><th>".$rep['meeting_date']." ".$rep['meeting_time']."</th><th>".$rep['meeting_location']."</th></tr>";
      }}
      $markup = $markup."</table>";
     }
