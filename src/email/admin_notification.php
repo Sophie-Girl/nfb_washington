3c176fc37e277e16691b6779951a4ad596bb1fd6
sconnell <sconnell@nfb.org>
2019-12-24 12:17:01 -0500

    email class

--- src/email/admin_notification.php
+++ src/email/admin_notification.php
@@ -14,4 +14,16 @@ class admin_notification extends base
         $langcode = \Drupal::currentUser()->getPreferredLangcode();
         $result = $mailManager->mail($module, $key, $to, $langcode, $params, $send);
     }
+    public function ratings_email_details($recipient_email)
+    {
+        $mailManager = \Drupal::service('plugin.manager.mail');
+        $module = 'nfb_washington';
+        $key = 'meeting_update';
+        $to = $recipient_email;
+        $send = true;
+        $params['message'] = $this->get_body();
+        $params['subject'] = $this->get_subject();
+        $langcode = \Drupal::currentUser()->getPreferredLangcode();
+        $result = $mailManager->mail($module, $key, $to, $langcode, $params, $send);
+    }
 }
