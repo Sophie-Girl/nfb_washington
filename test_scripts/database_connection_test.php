c62ce9f1f025804c6be7a5305e6638b3135e8f55
sconnell <sconnell@nfb.org>
2019-12-31 20:16:09 -0500

    Fix home markup

--- test_scripts/database_connection_test.php
+++ test_scripts/database_connection_test.php
@@ -3,4 +3,4 @@ Use Drupal\nfb_washington\archive_nfb\representative_data;
 $query = new representative_data();
 $state = "MD";
 $query->get_house_rep_for_state($state, $result);
-pinrt_r($result);
+print_r($result);
