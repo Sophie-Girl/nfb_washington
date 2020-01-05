e42ee9d6c9409f75f1c631c9b7f5f34ef3a9914d
sconnell <sconnell@nfb.org>
2020-01-05 02:26:35 -0500

    adding rep expected to attend field to home

--- src/form_factory/update_form_ajax_test.php
+++ src/form_factory/update_form_ajax_test.php
@@ -1,5 +1,6 @@
 <?php
 Namespace Drupal\nfb_washington\form_factory;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\nfb_washington\archive_nfb\representative_data;
 
 class update_form_ajax_test extends markup_elements
@@ -7,25 +8,28 @@ class update_form_ajax_test extends markup_elements
     public function field_set(&$form, $form_state)
     {
         $form['meeting_data'] = array(
-         '#prefix' => "<div id = 'data_wrapper'>",
+            '#prefix' => "<div id = 'data_wrapper'>",
             '#type' => 'fieldset',
             '#title' => $this->t("Meeting Information"),
-         '#suffix' => "</div>",
+            '#suffix' => "</div>",
         );
     }
+
     public function update_first_name(&$form, $form_state)
     {
         $this->representative_data = new representative_data();
         $category = 'nfb_contact_name';
         $this->representative_data->get_rep_data_update($form_state, $category, $text);
         $form['meeting_data']['nfb_civicrm_f_name_1'] = array(
-          '#type' => 'textfield',
-          '#title' => $this->t("Contact Person Name"),
-          '#value' =>   $text,
-          '#size' => 20,
-          '#required' => TRUE,
-        ); $this->representative_data = null;
+            '#type' => 'textfield',
+            '#title' => $this->t("Contact Person Name"),
+            '#value' => $text,
+            '#size' => 20,
+            '#required' => TRUE,
+        );
+        $this->representative_data = null;
     }
+
     public function update_contact_phone_record(&$form, $form_state)
     {
         $this->representative_data = new representative_data();
@@ -34,11 +38,13 @@ class update_form_ajax_test extends markup_elements
         $form['meeting_data']['nfb_civicrm_phone_1'] = array(
             '#type' => 'textfield',
             '#title' => $this->t("Contact Person Phone"),
-            '#value' =>  $text,
+            '#value' => $text,
             '#size' => 20,
             '#required' => TRUE,
-        ); $this->representative_data = null;
+        );
+        $this->representative_data = null;
     }
+
     public function update_meeting_location(&$form, $form_state)
     {
         $this->representative_data = new representative_data();
@@ -47,28 +53,32 @@ class update_form_ajax_test extends markup_elements
         $form['meeting_data']['meeting_location'] = array(
             '#type' => 'textfield',
             '#title' => $this->t("Meeting Location"),
-            '#value' =>  $text,
+            '#value' => $text,
             '#size' => 20,
             '#required' => TRUE,
-        ); $this->representative_data = null;
+        );
+        $this->representative_data = null;
     }
+
     public function update_meeting_date(&$form, $form_state)
     {
         $form['meeting_data']['meeting_day'] = array(
-            '#type'  => 'date',
+            '#type' => 'date',
             '#title' => $this->t("Meeting Date"),
             '#format' => "'m/d/Y'",
             '#required' => TRUE,
             '#date_year_range' => '-0: +0',
             '#date_date_form' => "'m/d/Y'",
-            '#min' =>   '2/1/2020',
+            '#min' => '2/1/2020',
             '#max' => '2/28/2020',
-        ); $this->representative_data = null;
+        );
+        $this->representative_data = null;
     }
 
     public function meeting_time_update_element(&$form, $form_state)
     {
-        $this->meeting_time_conversion($form_state, $text); $this->time_options();
+        $this->meeting_time_conversion($form_state, $text);
+        $this->time_options();
         $form['meeting_data']['meeting_time'] = array(
             '#type' => 'select',
             '#title' => "Meeting Time",
@@ -76,6 +86,33 @@ class update_form_ajax_test extends markup_elements
             '#value' => $text,
             '#required' => TRUE,
         );
+    }
+
+    public function update_expectend_attedence(&$form, $f0rm_state)
+    {
+        $this->get_yes_no_text($f0rm_state, $text);
+        $form['meeting_data']['attendance'] = array(
+            '#type' => 'select',
+            '#title' => "Meeting Time",
+            '#options' => array(
+                '1' => 'Yes',
+                '0' => 'No',
+            ),
+            '#value' => $text,
+            '#required' => TRUE,
+        );
+    }
+
+    public function get_yes_no_text(FormStateInterface $form_state, &$text)
+    {
+        $category = "contact_expected"; $this->representative_data = new representative_data();
+        $this->representative_data->get_rep_data_update($form_state, $category, $text);
+        if($text != "" && $text != "Error")
+        {
+            if($text = '1'){$text = "Yes";}
+            else{$text = "No";}
+        }
+        else {$text ="";} $this->representative_data = null;
     }
 
     public function meeting_time_conversion($form_state, &$text)
