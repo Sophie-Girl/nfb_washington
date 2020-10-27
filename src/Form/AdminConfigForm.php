edccb103fdd4017f2eafbc241be28bcf7c4525f6
sconnell <sconnell@nfb.org>
2020-10-27 13:53:44 -0400

    admin home page

--- src/Form/AdminConfigForm.php
+++ src/Form/AdminConfigForm.php
@@ -2,6 +2,7 @@
 Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\nfb_washington\form_factory\admin\admin_config;
 use Drupal\nfb_washington\verification\api_key_check;
 
 class AdminConfigForm extends FormBase
@@ -15,8 +16,9 @@ class AdminConfigForm extends FormBase
     public function buildForm(array $form, FormStateInterface $form_state)
     {
         $form['#attached']['library'][] = 'nfb_washington/nfb-washington';
-       $this->verification = new api_key_check();
+       $this->verification = new api_key_check(); $this->factory =  new admin_config();
        $this->verification->api_key_validation($form, $form_state);
+       $this->factory->build_form_array($form, $form_state);
        return $form;
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
---
+++ src/form_factory/admin/admin_config.php
@@ -0,0 +1,45 @@
+<?php
+namespace  Drupal\nfb_washington\form_factory\admin;
+class admin_config
+{
+    public function  API_key_markup(&$form, $form_state)
+    {
+        $form['explain_markup'] = array(
+          "#type" => "item",
+          "#markup" => "<p>The API Key is what allows us to make requests to Propublica's API for congressional data. 
+For information on th eAPi please visit their website <a href='https://projects.propublica.org/api-docs/congress-api/'>here.</a></p>"
+        );
+    }
+    public function APIKey_text_Field(&$form, $form_state)
+    {
+        $form['api_key'] = array(
+          '#type' => 'textfield',
+           '#size' => '20',
+           '#title' => "Propublica API Key (Required to pull Congressional data)",
+           '#required' => true,
+            '#max' => '50',
+        );
+    }
+    public function Congress_Number(&$form, $form_state)
+    {
+        $form['congress_number'] = array(
+          '#type' => 'select',
+          '#title' => "Congress Number (i.e 116th Congress)",
+          "#options" => array("115" => "115th",
+                              "116" => "116th",
+              "117" => "117th",
+              "118" => "118th",
+              '119' => "119th",
+              "120" => "120th"
+              ),
+          "#required" => "true",
+        );
+    }
+    public function build_form_array(&$form, $form_state)
+    {
+        $this->API_key_markup($form, $form_state);
+        $this->APIKey_text_Field($form, $form_state);
+        $this->Congress_Number($form, $form_state);
+    }
+
+}
