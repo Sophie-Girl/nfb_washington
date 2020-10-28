3afb0a699ad67f23a54c2e6b7fd6f0876fc2e8b4
sconnell <sconnell@nfb.org>
2020-10-28 19:29:08 -0400

    debug

--- src/Form/AdminMemberForm.php
+++ src/Form/AdminMemberForm.php
@@ -3,6 +3,7 @@ Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\nfb_washington\verification\api_key_check;
+use Drupal\nfb_washington\verification\congress_number_check;
 
 class AdminMemberForm extends FormBase
 {
@@ -15,6 +16,7 @@ class AdminMemberForm extends FormBase
     {
         $form['#attached']['library'][] = 'nfb_washington/nfb-washington';
         $this->api_verification($form, $form_state);
+        $this->congress_number_markup($form, $form_state);
 
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
@@ -25,5 +27,10 @@ class AdminMemberForm extends FormBase
     {
         $this->verification = new api_key_check();
         $this->verification->api_key_validation($form, $form_state);
+    }
+    public function congress_number_markup(&$form, $form_state)
+    {
+        $this->verification = new congress_number_check();
+        $this->verification->congress_number_verification($form, $form_state);
     }
 }
