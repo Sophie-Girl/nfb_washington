fa9c598d1d41eb2e5f444e8cdc386265913965fb
sconnell <sconnell@nfb.org>
2020-10-28 19:43:47 -0400

    debug

--- src/Form/AdminMemberForm.php
+++ src/Form/AdminMemberForm.php
@@ -17,6 +17,7 @@ class AdminMemberForm extends FormBase
         $form['#attached']['library'][] = 'nfb_washington/nfb-washington';
         $this->api_verification($form, $form_state);
         $this->congress_number_markup($form, $form_state);
+        \drupal::logger("nfb_Washington")->notice(print_r($form, true));
         return $form;
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
