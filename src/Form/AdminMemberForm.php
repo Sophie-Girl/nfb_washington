9177b3e4b8c17ccebb5519645de5ef6b53dc5ee4
sconnell <sconnell@nfb.org>
2020-10-28 11:54:31 -0400

    admin configuration

--- src/Form/AdminMemberForm.php
+++ src/Form/AdminMemberForm.php
@@ -2,20 +2,27 @@
 Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\nfb_washington\verification\api_key_check;
 
 class AdminMemberForm extends FormBase
 {
     public $verification;
     public function  getFormId()
     {
-        // TODO: Implement getFormId() method.
+        return "nfb_wash_admin_member_form";
     }
     public function buildForm(array $form, FormStateInterface $form_state)
     {
-        // TODO: Implement buildForm() method.
+        $form['#attached']['library'][] = 'nfb_washington/nfb-washington';
+
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
         // TODO: Implement submitForm() method.
+    }
+    public function api_verification(&$form, $form_state)
+    {
+        $this->verification = new api_key_check();
+        $this->verification->api_key_validation($form, $form_state);
     }
 }
