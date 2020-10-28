fa728255386e2538420b0ff2394cbbc639af4b29
sconnell <sconnell@nfb.org>
2020-10-28 11:30:37 -0400

    admin configuration

---
+++ src/Form/AdminMemberForm.php
@@ -0,0 +1,21 @@
+<?php
+Namespace Drupal\nfb_washington\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class AdminMemberForm extends FormBase
+{
+    public $verification;
+    public function  getFormId()
+    {
+        // TODO: Implement getFormId() method.
+    }
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        // TODO: Implement buildForm() method.
+    }
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        // TODO: Implement submitForm() method.
+    }
+}
