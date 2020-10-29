623e41de35ef7f192fb28c71c3733b22ab2a7147
sconnell <sconnell@nfb.org>
2020-10-29 12:17:48 -0400

    debug

--- src/Form/AdminMemberForm.php
+++ src/Form/AdminMemberForm.php
@@ -2,12 +2,14 @@
 Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\nfb_washington\form_factory\admin\admin_members;
 use Drupal\nfb_washington\verification\api_key_check;
 use Drupal\nfb_washington\verification\congress_number_check;
 
 class AdminMemberForm extends FormBase
 {
     public $verification;
+    public $factory;
     public function  getFormId()
     {
         return "nfb_wash_admin_member_form";
@@ -17,6 +19,8 @@ class AdminMemberForm extends FormBase
         $form['#attached']['library'][] = 'nfb_washington/nfb-washington';
         $this->api_verification($form, $form_state);
         $this->congress_number_markup($form, $form_state);
+        $this->factory = new admin_members();
+        $this->factory->build_form_array($form, $form_state);
         return $form;
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
@@ -32,5 +36,9 @@ class AdminMemberForm extends FormBase
     {
         $this->verification = new congress_number_check();
         $this->verification->congress_number_verification($form, $form_state);
+    }
+    public function markup_reset(&$form, &$form_state)
+    {
+        return$form['mode_explain'];
     }
 }
---
+++ src/form_factory/admin/admin_members.php
@@ -0,0 +1,57 @@
+<?php
+namespace  Drupal\nfb_washington\form_factory\admin;
+use Drupal\core\form\FormStateInterface;
+
+class admin_members
+{
+    public function build_form_array(&$form, FormStateInterface &$form_state)
+    {
+        $this->build_mode_select($form, $form_state);
+        $this->build_markup($form, $form_state);
+    }
+    public function build_mode_select(&$form, FormStateInterface &$form_state)
+    {
+        $form['members_mode']= array(
+            '#type' => "select",
+            "#title" => "Members of Congress Mode",
+            '#required' => true,
+            '#options' => array(
+              "initial_upload" => "Import New Congress",
+              "maintenance" => "Maintain current Congressional records"
+            ),
+            "#ajax" => array(
+                "event" => "change",
+                "callback" => 'markup_reset',
+                'wrapper' => "explain_markup"
+            )
+        );
+        $form['submit'] = array(
+            '#type' => 'submit',
+            '#value' => "Submit",);
+    }
+    public function build_markup(&$form, FormStateInterface &$form_state)
+    {
+        $form['mode_explain'] = array(
+          "#prefix" => "<div id = 'explain_markup'>",
+          "#type" => "item",
+          '#markup' => $this->markup_choice($form_state),
+          "#suffix" => "</div>",
+        );
+    }
+    public function markup_choice(FormStateInterface &$form_state)
+    {
+        if($form_state->getValue("members_mode'")== "")
+        {$markup = "<p>Please select a mode</p>";}
+        elseif($form_state->getValue("members_mode'")== "initial_upload")
+        {$markup = "<p><b>Initial Upload:</b> This will upload new members of congress, 
+           while removing any members form the previous congress who resigned, did not 
+           win reelection, or left office under other circumstances. Should be run after 
+           the election of a new congress</p>";}
+        else {
+            $markup = "<p><b>Maintenance:</b> Checks the current congress for any members hwo have left 
+            Congress. As well as important any people who ahve entered office due to special elections, 
+            or other means. </p>";
+        }
+        return $markup;
+    }
+}
