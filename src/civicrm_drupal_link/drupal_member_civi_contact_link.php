91aa5c508f44485b681dfe06cdc9c29972df262e
sconnell <sconnell@nfb.org>
2020-10-30 16:40:31 -0400

    setting Up civi_query

--- src/civicrm_drupal_link/drupal_member_civi_contact_link.php
+++ src/civicrm_drupal_link/drupal_member_civi_contact_link.php
@@ -2,5 +2,9 @@
 Namespace Drupal\nfb_washington\civicrm_drupal_link;
 class drupal_member_civi_contact_link
 {
-
+    public $civi_query;
+    public $propublica_query;
+    public function  __construct()
+    {
+    }
 }
