40f92630b641f2826745e48ca0744a2133299e1a
sconnell <sconnell@nfb.org>
2020-11-06 13:36:08 -0500

    debug

--- src/post_process/admin/admin_member_backend.php
+++ src/post_process/admin/admin_member_backend.php
@@ -7,13 +7,14 @@ class admin_member_backend
     public $link;
     public function __construct(drupal_member_civi_contact_link $drupal_member_civi_contact_link)
     {
-        ini_set('max_execution_time', 500);
+
         $this->link = $drupal_member_civi_contact_link;
     }
     public function backend($mode)
     {
-        if($mode == "new_congress")
-        $this->link->new_congress_run_through();
+        ini_set('max_execution_time', 500);
+        if($mode == "new_congress"){
+        $this->link->new_congress_run_through();}
         else
         {$this->link->mid_congress_maint();}
     }
