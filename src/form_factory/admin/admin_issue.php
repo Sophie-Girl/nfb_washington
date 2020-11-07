d51fb1b3ae6d91e56c3832f3602eabe369fecdbb
sconnell <sconnell@nfb.org>
2020-11-07 17:33:53 -0500

    debug

--- src/form_factory/admin/admin_issue.php
+++ src/form_factory/admin/admin_issue.php
@@ -2,5 +2,17 @@
 namespace  Drupal\nfb_washington\form_factory\admin;
 class admin_issue
 {
+    public $database;
+
+    public function issue_switch($issue)
+    {
+       if($issue == "create")
+       {
+           // todo create new issue
+       }
+       else {
+           // todo edit specific issue
+       }
+    }
 
 }
