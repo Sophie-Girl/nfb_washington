f05122be63ac65890da3cd5f8224ae174307a898
sconnell <sconnell@nfb.org>
2020-11-09 08:52:48 -0500

    debug

--- src/form_factory/admin/admin_issue.php
+++ src/form_factory/admin/admin_issue.php
@@ -4,7 +4,7 @@ class admin_issue
 {
     public $database;
 
-    public function issue_switch($issue, &$form, $form_state)
+    public function issue_switch($issue, &$form, &$form_state)
     {
        if($issue == "create")
        {
@@ -18,6 +18,33 @@ class admin_issue
     {
     }
     public function edit_existing($issue, &$form, $form_state)
+    {
+
+    }
+    public function build_issue_name(&$form, $form_state)
+    {
+        $form["create_issue_name"] = array(
+            '#type' => 'textfield',
+            '#title' => "Issue Name",
+            '#required' => true,
+            '#size' => "20",
+            '#min' => 4,
+            '#max' => 80,
+        );
+    }
+    public function create_edit_issue_name($issue, &$form, &$form_state)
+    {
+        $form["edit_issue_name"] = array(
+            '#type' => 'textfield',
+            '#title' => "Issue Name",
+            '#required' => true,
+            '#size' => "20",
+            '#min' => 4,
+            '#max' => 80,
+        );
+        $form_state['input']['edit_edit_issue_name'] = $issue['name'];
+    }
+    public function create_issue__bill(&$form, $form_State)
     {
 
     }
