7c62b1b720811a9c5bdd6c72c8de57ce43d18c58
sconnell <sconnell@nfb.org>
2020-11-10 16:06:41 -0500

    css and start of issue home form

--- src/form_factory/admin/admin_issue_home.php
+++ src/form_factory/admin/admin_issue_home.php
@@ -1,13 +1,16 @@
 <?php
 namespace  Drupal\nfb_washington\form_factory\admin;
+use Drupal\nfb_washington\database\base;
+
 class admin_issue_home
 {
     public $database;
     public function Build_issue_home_form(&$form, $form_state)
     {
-
+        $this->initial_element($form, $form_state);
+        $this->issue_table($form, $form_State);
     }
-    public function initial_markup(&$form, $form_state)
+    public function initial_markup()
     {
         $year = date("Y");
         $markup = "<p>Issues that are currently being brought up during the ".$year." Washington
@@ -17,9 +20,56 @@ class admin_issue_home
      <p><a href='/nfb_washington/admin/issue/create' class = 'button-1'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Creave Issue&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></p>";
         return $markup;
     }
+    public function initial_element(&$form, $form_state)
+    {
+        $form['header_markup'] = array(
+            '#type' => 'item',
+            '#markup' => $this->initial_markup()
+        );
+    }
     public function issue_table(&$form, $form_state)
     {
-
+        $this->table_query($result);
+        $this->create_table($markup, $result);
+        $form['issue_table'] = array(
+          '#type' => "item",
+          '#markup' => $markup,
+        );
+    }
+    public function table_query(&$result)
+    {
+        $this->database = new base();
+        $query = "select * from nfb_washington_issues order by issue_year desc limit 12;";
+        $key = 'issue_id';
+        $this->database->select_query($query, $key);
+        $result = $this->database->get_result();
+    }
+    public function create_table(&$markup, $result)
+    {
+        $count = 1;
+        $markup = "<table width='100%'>
+<t><th class='table-header'>Issue Name</th><th class='table-header'>Bill Slug</th><th class='table-header'>First Use</th><th class='table-header'>Actions</th></tr>";
+        foreach($result as $issue)
+        {
+            $issue = get_object_vars($issue);
+            if($issue['primary_status'] == "0")
+            {$primary = "Yes";} else {$primary = "No";}
+            $markup = $markup."<tr><td>".$issue['issue_name']."</td><td>".$issue['bill_slug']."</td><td>".$primary."</td><td><a href='/nfb_washington/admin/issues/".$issue['issue_id']."' class='button-".$count."'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Edit/View&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a> </td></tr>";
+            $this->count_catch($count);
+        }
+        $markup = $markup."</table>";
+    }
+    public function count_catch(&$count)
+    {
+        if($count == 1)
+        {
+            $count = 2;
+        }
+        elseif($count == 2)
+        {
+            $count = 3;
+        }
+        else{ $count = 1;}
     }
 
 
