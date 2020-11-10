755af5bb4b8b9ec4ab2df940f97ea5f3605af631
sconnell <sconnell@nfb.org>
2020-11-10 09:59:55 -0500

    issue form backend

--- src/post_process/admin/admin_issue_backend.php
+++ src/post_process/admin/admin_issue_backend.php
@@ -8,12 +8,21 @@ class admin_issue_backend
     public $primary_issue;
     public $created_user;
     public $modified_user;
+    public $bill_id;
+    public $bill_slug;
+    public $primary_issue_id;
     public function get_primary_issue()
     {return $this->primary_issue;}
     public function get_created_user()
     {return $this->created_user;}
     public function get_modified_user()
     {return $this->modified_user;}
+    public function get_bill_id()
+    {return $this->bill_id;}
+    public function  get_bill_slug()
+    {return $this->bill_slug;}
+    public function get_primary_issue_id()
+    {return $this->primary_issue_id;}
     public function issue_backend($issue, FormStateInterface $form_state)
     {
 
@@ -36,5 +45,30 @@ class admin_issue_backend
         $user = \drupal::currentUser()->getUsername();
         $this->modified_user = $user;
     }
+    public function issue_switch($issue, FormStateInterface $form_state)
+    {
+        if($issue = "create")
+        {
+
+        }
+        else
+        {
+
+        }
+    }
+    public function create_backend(FormStateInterface $form_state)
+    {
+        $fields = array(
+            'issue_name'  => $form_state->getValue("issue_name"),
+            "bill_id" => $this->get_bill_id(),
+            "bill_slug" => $this->get_bill_slug(),
+            "civicrm_id" => "0",
+            "primary_issue" => $this->get_primary_issue(),
+            "primary_issue_id" => $this->get_primary_issue_id(),
+            "created_user" => $this->get_created_user(),
+            "modified_user" => $this->get_modified_user(),
+        );
+    }
+
 
 }
