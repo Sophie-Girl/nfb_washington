8ef78c594b01a4e20dd2f4f891f1c859f81e1304
sconnell <sconnell@nfb.org>
2020-11-11 13:23:01 -0500

    css and start of issue home form

--- js/edit-issue.js
+++ js/edit-issue.js
@@ -9,7 +9,12 @@
                         url: '/nfb_washington/admin/ajax/issue',
                         data: {route: issueid},
                     }).done(function (data) {
-
+                        var issue = data;
+                        document.getElementById('edit-issue-name').value = issue[0];
+                       document.getElementById('edit-bill-id').value = issue[1];
+                       document.getElementById('edit-bill-slug').value = issue[2];
+                       document.getElementById('edit-primary-issue').value = setting[3];
+                       document.getElementById('edit-derivative-issue').value = setting[4];
                     });
                 }
             }
