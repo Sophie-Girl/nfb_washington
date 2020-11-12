827e60bef2a2e6c1722c05c7fd38c8980d6c4ec1
sconnell <sconnell@nfb.org>
2020-11-12 15:50:46 -0500

    add issue committee link table to schema (only on install)

--- src/Form/AdminCommittHomeForm.php
+++ src/Form/AdminCommittHomeForm.php
@@ -2,12 +2,14 @@
 Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\nfb_washington\form_factory\admin\admin_committee_home;
 use Drupal\nfb_washington\verification\api_key_check;
 use Drupal\nfb_washington\verification\congress_number_check;
 
 class AdminCommittHomeForm extends FormBase
 {
     public $verification;
+    public $form_factory;
     public function  getFormId()
     {
         return "nfb_washington_admin_commit_home";
@@ -16,6 +18,8 @@ class AdminCommittHomeForm extends FormBase
     {
         $this->verify_api_key($form, $form_state);
         $this->congress_number_markup($form, $form_state);
+        $this->form_factory = new admin_committee_home();
+        $this->form_factory->build_com_home_form($form, $form_state);
         return $form;
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
