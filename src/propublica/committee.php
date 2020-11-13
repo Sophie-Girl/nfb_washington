4da788575279bd6c3e8d4c55982dffa6f48db5a5
sconnell <sconnell@nfb.org>
2020-11-13 18:02:54 -0500

    add issue committee link table to schema (only on install)

--- src/propublica/committee.php
+++ src/propublica/committee.php
@@ -2,5 +2,17 @@
 Namespace Drupal\nfb_washington\propublica;
 class committee extends members
 {
+    public $committee_id;
+    public function get_committee_id()
+    {return $this->committee_id;}
+    public function general_committee_search()
+    {
+        $this->api_url = "https://api.propublica.org/congress/v1/".$this->get_congress_number()."/".$this->get_search_criteria_1()."/".$this->get_entity().".json";
+    }
+    public function specific_committee_search()
+    {
+        $this->api_url = "https://api.propublica.org/congress/v1/".$this->get_congress_number()."/".$this->get_search_criteria_1()."/".$this->get_entity()."/".$this->get_committee_id().".json";
+    }
+
 
 }
