3af9eec342bcc1fae65f6a5b3c39db2ad23d90e3
sconnell <sconnell@nfb.org>
2020-11-14 10:35:14 -0500

    start committee backend

--- src/civicrm_drupal_link/drupal_propublica_committee_link.php
+++ src/civicrm_drupal_link/drupal_propublica_committee_link.php
@@ -1,6 +1,12 @@
 <?php
 Namespace Drupal\nfb_washington\civicrm_drupal_link;
+use Drupal\nfb_washington\propublica\committee;
+
 class drupal_propublica_committee_link
 {
-
+    public $propublica;
+    public function __construct(committee $committee)
+    {
+        $this->propublica = $committee;
+    }
 }
