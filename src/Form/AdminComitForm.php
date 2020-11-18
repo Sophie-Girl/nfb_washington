033982f46622169cde26f1299d91ea52b5897106
sconnell <sconnell@nfb.org>
2020-11-18 15:02:05 -0500

    start committee backend

--- src/Form/AdminComitForm.php
+++ src/Form/AdminComitForm.php
@@ -2,6 +2,7 @@
 Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\nfb_washington\database\base;
 use Drupal\nfb_washington\form_factory\admin\admin_committee;
 use Drupal\nfb_washington\post_process\admin\admin_committee_backend;
 use Drupal\nfb_washington\verification\api_key_check;
@@ -12,6 +13,7 @@ class AdminComitForm extends FormBase
     public $verification;
     public $form_factory;
     public $backend;
+    public $database;
     public function getFormId()
     {
         return "nfb_washington_admin_commit";
@@ -42,5 +44,22 @@ class AdminComitForm extends FormBase
         $this->verification = new congress_number_check();
         $this->verification->congress_number_verification($form, $form_state);
         $this->verification = null;
+    }
+    public function validateForm(array &$form, FormStateInterface $form_state)
+    {
+        parent::validateForm($form, $form_state);
+        $this->database = new base();
+        $query = "select * from nfb_washington_committee where committee_name = '".$form_state->getValue("committee_name")."'
+        and committee_id != '".$form_state->getValue("committee_value")."' and chamber = '".$form_state->getValue("chamber")."';";
+        $key = "committee_id";
+        $this->database->select_query($query, $key);
+        $count = 0;
+        foreach ($this->database->get_result() as $committee)
+        {
+            $count++;
+        }
+        if($count > 0)
+        {$form_state->setErrorByName("committee_name", "THis committee already exists");}
+
     }
 }
