a9699a629885f87d9db1e618da4be84ded8ab917
sconnell <sconnell@nfb.org>
2020-11-18 17:05:02 -0500

    start committee backend

--- src/civicrm_drupal_link/drupal_propublica_committee_link.php
+++ src/civicrm_drupal_link/drupal_propublica_committee_link.php
@@ -151,6 +151,7 @@ class drupal_propublica_committee_link
     }
     public function member_committee_run_through()
     {
+        \drupal::logger("nfb_washington_debug")->notice(print_r($this->propublica->get_propublica_result(), true));
         foreach($this->propublica->get_propublica_result()['result']['0']['current_members'] as $com_mem)
         {
             $this->drupal_member_id = null;
