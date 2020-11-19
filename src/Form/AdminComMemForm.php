fd5ccc36b2fce7f7e5c687bdfae4087d9717aba4
sconnell <sconnell@nfb.org>
2020-11-19 13:51:10 -0500

    start committee backend

--- src/Form/AdminComMemForm.php
+++ src/Form/AdminComMemForm.php
@@ -2,6 +2,8 @@
 Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\nfb_washington\verification\api_key_check;
+use Drupal\nfb_washington\verification\congress_number_check;
 
 class  AdminComMemForm extends FormBase
 {
@@ -11,10 +13,24 @@ class  AdminComMemForm extends FormBase
     }
     public function buildForm(array $form, FormStateInterface $form_state)
     {
-        // TODO: Implement buildForm() method.
+        $form['#attached']['library'][] = 'nfb_washington/nfb-washington';
+        $this->verify_api_key($form, $form_state);
+        $this->congress_number_markup($form, $form_state);
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
         // TODO: Implement submitForm() method.
+    }
+    public function verify_api_key(&$form, $form_state)
+    {
+        $this->verification = new api_key_check();
+        \drupal::logger('nfb_washington')->notice("i am about to run the query");
+        $this->verification->api_key_validation($form,$form_state);
+    }
+    public function congress_number_markup(&$form, &$form_state)
+    {
+        $this->verification = new congress_number_check();
+        $this->verification->congress_number_verification($form, $form_state);
+        $this->verification = null;
     }
 }
