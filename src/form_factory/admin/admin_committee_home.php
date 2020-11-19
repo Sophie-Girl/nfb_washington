e596830bd0b16d6c8c38df5da1b87938329470d7
sconnell <sconnell@nfb.org>
2020-11-19 07:57:29 -0500

    start committee backend

--- src/form_factory/admin/admin_committee_home.php
+++ src/form_factory/admin/admin_committee_home.php
@@ -49,7 +49,7 @@ linked to an issue
         {
 
             $committee = get_object_vars($committee);
-            $markup = $markup."<tr><td>".$committee['committee_name']."</td><td>".$committee['propublica_id']."</td><td><a href='nfb_washington/admin/committee/".$committee['committee_id']."' class='button-".$count."'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Edit&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a> </td></tr>";
+            $markup = $markup."<tr><td>".$committee['committee_name']."</td><td>".$committee['propublica_id']."</td><td><a href='/nfb_washington/admin/committee/".$committee['committee_id']."' class='button-".$count."'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Edit&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a> </td></tr>";
             $this->count_switch($count);
         }
         $markup = $markup."</table>";
