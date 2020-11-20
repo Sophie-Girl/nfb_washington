fbdc1d4ad572ee3a440163e6fd236d7eea32f130
sconnell <sconnell@nfb.org>
2020-11-20 09:52:51 -0500

    start committee backend

--- src/Form/AdminComMemForm.php
+++ src/Form/AdminComMemForm.php
@@ -3,6 +3,7 @@ Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\nfb_washington\form_factory\admin\admin_com_member;
+use Drupal\nfb_washington\post_process\admin\admin_committee_backend;
 use Drupal\nfb_washington\verification\api_key_check;
 use Drupal\nfb_washington\verification\congress_number_check;
 
@@ -10,6 +11,7 @@ class  AdminComMemForm extends FormBase
 {
     public $verification;
     public $factory;
+    public $backend;
     public function getFormId()
     {
         return "nfb_washington_con_mem";
@@ -26,7 +28,8 @@ class  AdminComMemForm extends FormBase
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
-        // TODO: Implement submitForm() method.
+        $this->backend = new admin_committee_backend();
+        $this->backend->member_backend($form_state);
     }
     public function verify_api_key(&$form, $form_state)
     {
