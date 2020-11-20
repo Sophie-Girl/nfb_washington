9b84eb76bcede794ac17d6d00730b9c394bce93e
sconnell <sconnell@nfb.org>
2020-11-20 11:25:23 -0500

    start committee issue form

--- src/Form/AdminComIsLinkForm.php
+++ src/Form/AdminComIsLinkForm.php
@@ -2,9 +2,13 @@
 Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\nfb_washington\verification\api_key_check;
+use Drupal\nfb_washington\verification\congress_number_check;
 
 class  adminComIsLinkForm extends  FormBase
 {
+    public  $verification;
+
     public function getFormId()
     {
         // TODO: Implement getFormId() method.
@@ -16,5 +20,16 @@ class  adminComIsLinkForm extends  FormBase
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
         // TODO: Implement submitForm() method.
+    }
+    public function verify_api_key(&$form, $form_state)
+    {
+        $this->verification = new api_key_check();
+        $this->verification->api_key_validation($form,$form_state);
+    }
+    public function congress_number_markup(&$form, &$form_state)
+    {
+        $this->verification = new congress_number_check();
+        $this->verification->congress_number_verification($form, $form_state);
+        $this->verification = null;
     }
 }
