ce349ac74dba81582125b02253b38369c9b50f38
sconnell <sconnell@nfb.org>
2020-11-23 16:54:17 -0500

    start committee issue form

--- src/Form/AdminCreateNoteForm.php
+++ src/Form/AdminCreateNoteForm.php
@@ -2,21 +2,27 @@
 Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use drupal\nfb_washington\form_factory\admin\admin_note_create;
 use Drupal\nfb_washington\verification\api_key_check;
 use Drupal\nfb_washington\verification\congress_number_check;
 
 class AdminCreateNoteForm extends FormBase
 {
     public  $verification;
+    public $form_factory;
     public function getFormId()
     {
        return "nfb_Washington_note_creation_form";
     }
-    public function buildForm(array $form, FormStateInterface $form_state)
+    public function buildForm(array $form, FormStateInterface $form_state, $note = 'create')
     {
         $form['#attached']['library'][] = 'nfb_washington/nfb-washington';
         $this->verify_api_key($form, $form_state);
         $this->congress_number_markup($form, $form_state);
+        $this->form_factory = new admin_note_create();
+        $this->form_factory->build_form_Array($form, $form_state, $note);
+        $this->form_factory = null;
+        return $form;
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
--- src/form_factory/admin/admin_note_create.php
+++ src/form_factory/admin/admin_note_create.php
@@ -1,6 +1,61 @@
 <?php
 namespace  Drupal\nfb_washington\form_factory\admin;
+use Drupal\Core\Form\FormStateInterface;
+
 class admin_note_create
 {
+    public function build_form_Array(&$form, FormStateInterface $form_state, $note)
+    {
+        $this->hidden_note_value($form, $form_state, $note);
+        $this->note_year($form, $form_state);
+        $this->note_type_element($form, $form_state);
+        $this->note_text($form, $form_state);
+        $form['submit'] = array(
+            '#type' => 'submit',
+            '#value' => "Submit",
+        );
+    }
+    public function note_type_element(&$form, $form_state)
+    {
+        $form['note_type'] = array(
+          '#type' => 'select',
+          '#title' => "Select Note Type",
+            '#required' => true,
+            '#options' => array(
+                "bill_sponsor" => "Bill Sponsor",
+                "bill_co_sponsor" => "Bill Co-Sponsor",
+                "state_convention" => "Spoke at a State Convention",
+                "national_convention" => " Spoke at National convention")
+        );
+    }
+    public function note_text(&$form, $form_state)
+    {
+        $form['note_text'] =- array(
+          "#type" => "textarea",
+          "#title" => "Note Text",
+          "#required" => true,
+          "#max" => 500,
+          "#min" => 1,
+        );
+    }
+    public function hidden_note_value(&$form, $form_state, $note)
+    {
+        $form['note_value'] = array(
+            '#type' => 'textfield',
+            '#value' => $note,
+            '#size' => '20',
+            '#attributes' => array('readonly' => 'readonly'),
+            '#title' => "Issue Id"
+        );
+    }
+    public function note_year(&$form, $form_state)
+    {
+        $form['note_year'] = array(
+            '#type' => 'textfield',
+            '#etitle' => "Year",
+            '#max' => 5,
+            '#required' => true
+        );
+    }
 
 }
