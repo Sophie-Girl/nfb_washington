6466e76f3f3d7cdc25bcc42aff7537889e4c4236
sconnell <sconnell@nfb.org>
2020-11-24 12:22:13 -0500

    start committee issue form

--- src/Form/AdminNoteHomeForm.php
+++ src/Form/AdminNoteHomeForm.php
@@ -2,19 +2,35 @@
 Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\nfb_washington\verification\api_key_check;
+use Drupal\nfb_washington\verification\congress_number_check;
 
 class AdminNoteHomeForm extends FormBase
 {
+    public $verification;
+
     public function  getFormId()
     {
         return "admin_note_home";
     }
     public function buildForm(array $form, FormStateInterface $form_state)
     {
-        // TODO: Implement buildForm() method.
+       $this->verify_api_key($form, $form_state);
+       $this->congress_number_markup($form, $form_state);
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
         // TODO: Implement submitForm() method.
+    }
+    public function verify_api_key(&$form, $form_state)
+    {
+        $this->verification = new api_key_check();
+        $this->verification->api_key_validation($form,$form_state);
+    }
+    public function congress_number_markup(&$form, &$form_state)
+    {
+        $this->verification = new congress_number_check();
+        $this->verification->congress_number_verification($form, $form_state);
+        $this->verification = null;
     }
 }
