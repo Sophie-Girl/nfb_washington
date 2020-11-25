ae1b2dd4556731f76aa42e01f8620f3abf291f43
sconnell <sconnell@nfb.org>
2020-11-25 18:00:36 -0500

    start committee issue form

--- src/form_factory/admin/admin_note_link.php
+++ src/form_factory/admin/admin_note_link.php
@@ -1,1 +1,16 @@
 <?php
+namespace  Drupal\nfb_washington\form_factory\admin;
+use Drupal\Core\Form\FormStateInterface;
+
+class admin_note_link
+{
+    public $database;
+    public function build_form_array(&$form, FormStateInterface $form_state)
+    {
+
+    }
+    public function state_select( &$form, $form_state)
+    {
+
+    }
+}
