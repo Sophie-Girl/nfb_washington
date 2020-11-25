e388f6e22a5dee2e980ac581264c80aea9605920
sconnell <sconnell@nfb.org>
2020-11-25 09:22:00 -0500

    start committee issue form

---
+++ src/Controller/NoteEditAjaxController.php
@@ -0,0 +1,11 @@
+<?php
+Namespace Drupal\nfb_washington\Controller;
+use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class NoteEditAjaxController extends ControllerBase
+{
+
+}
