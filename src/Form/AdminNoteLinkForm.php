cc2569cb6f17f88156145848d32c84fc273d6696
sconnell <sconnell@nfb.org>
2020-11-26 16:44:26 -0500

    start committee issue form

--- src/Form/AdminNoteLinkForm.php
+++ src/Form/AdminNoteLinkForm.php
@@ -2,12 +2,14 @@
 Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\nfb_washington\form_factory\admin\admin_note_link;
 use Drupal\nfb_washington\verification\api_key_check;
 use Drupal\nfb_washington\verification\congress_number_check;
 
 class AdminNoteLinkForm extends FormBase
 {
     public $verification;
+    public $form_factory;
     public function getFormId()
     {
         return 'nfb_washington_note_link';
@@ -17,7 +19,9 @@ class AdminNoteLinkForm extends FormBase
         $form['#attached']['library'][] = 'nfb_washington/nfb-washington';
         $this->verify_api_key($form, $form_state);
         $this->congress_number_markup($form, $form_state);
-
+        $this->form_factory = new admin_note_link();
+        $this->form_factory->build_form_array($form, $form_state);
+        $this->form_factory = null;
         return $form;
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
@@ -34,5 +38,9 @@ class AdminNoteLinkForm extends FormBase
         $this->verification = new congress_number_check();
         $this->verification->congress_number_verification($form, $form_state);
         $this->verification = null;
+    }
+    public function member_refresh(&$form, &$form_state)
+    {
+        return $form['member'];
     }
 }
