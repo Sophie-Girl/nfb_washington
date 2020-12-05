79ca318db3cc588115dd08b656c3d0e52ad2ba91
sconnell <sconnell@nfb.org>
2020-12-05 10:25:00 -0500

    start committee issue form

--- src/form_factory/admin/admin_config.php
+++ src/form_factory/admin/admin_config.php
@@ -52,6 +52,7 @@ For information on th eAPi please visit their website <a href='https://projects.
         $this->API_key_markup($form, $form_state);
         $this->APIKey_text_Field($form, $form_state);
         $this->Congress_Number($form, $form_state);
+        $this->seminar_type($form, $form_state);
         $form['submit'] = array(
             '#type' => 'submit',
             '#value' => "Submit",);
