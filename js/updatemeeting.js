c50e0c2960c61372d3405a165f90ea778fc2ffaf
sconnell <sconnell@nfb.org>
2020-12-09 16:29:59 -0500

    start committee issue form

--- js/updatemeeting.js
+++ js/updatemeeting.js
@@ -1,27 +1,25 @@
-(function ($, Drupal) {
-    Drupal.behaviors.wash_sem_update_meeting = {
-        attach: function(context, settings){
-            $('#nfb-select-rep').once.onchange(function () {
-
-                $.ajax({
-                    type: 'POST',
-                    url: '/nfb_washington/ajax/meeting',
-                    data: { route: vroute },
-                }).done(function (data) {
-                    var meeting = data;
-                    console.log(toString(meeting));
-                    if(meetng[0] != "no_result")
-                    {
-                        if(meeting[0] != "null") {document.getElementById('edit-nfb-contact-name').value = meeting[0];}
-                        if(meeting[1] != "null") {document.getElementById("edit-nfb-civicrm-phone_1").value = meeting[1];}
-                        if(meeting[2] != "null") {document.getElementById('edit-meeting-location').value = meeting[2];}
-                        if(meeting[6] != "null"){document.getElementById('edit-in-memory-template-id').value = setting[6];}
-                        if(meeting[7] != "null"){document.getElementById('edit-attendance').value = meeting[7];}
-                        if(meeting[8] != "null"){document.getElementById('edit-moc-contact').value = meeting[8];}
-                    }
-
-                });});
-
+((function ($, Drupal) {
+    Drupal.behaviors.wash_sem_note_edit = {
+        attach: function(context, settings) {
+            window.onload = function () {
+                var vnoteid = $('#edit-note-value').val();
+                if (vnoteid != "create") {
+                    $.ajax({
+                        type: 'POST',
+                        url: '/nfb_washington/admin/ajax/note',
+                        data: { noteid:vnoteid },
+                    }).done(function (data) {
+                        var issue = data;
+                        document.getElementById('edit-note-type').value = issue[0];
+                        document.getElementById('edit-note-year').value = issue[1];
+                        document.getElementById('edit-note-text').value = issue[2];
+                    });
+                }
+                else{
+                    document.getElementById('edit-note-value').style.display = 'none';
+                    document.getElementsByClassName("form-item js-form-item form-type-textfield js-form-type-textfield form-item-note-value js-form-item-note-value")['0'].style.display = 'none';
+                }
+            }
         }
     }
-})(jQuery, Drupal);
+})(jQuery, Drupal)
