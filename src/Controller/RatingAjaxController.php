1a53c23e6e5447e1ecf899003fad329a1e5b5aae
sconnell <sconnell@nfb.org>
2020-12-10 12:10:02 -0500

    start committee issue form

--- src/Controller/RatingAjaxController.php
+++ src/Controller/RatingAjaxController.php
@@ -1,67 +1,107 @@
 <?php
 Namespace Drupal\nfb_washington\Controller;
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\nfb_washington\archive_nfb\activity_data;
+use Drupal\nfb_washington\database\base;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class RatingAjaxController extends ControllerBase
+class  IssueEditAJaxController extends  ControllerBase
 {
- public $archive;
- public $rep_id;
- public $ratings_array;
- public $result_array;
- public function  get_rep_id()
- {return $this->rep_id;}
- public function get_ratings_array()
- {return $this->ratings_array;}
- public function get_results_array()
- {return $this->result_array;}
- public function content()
- {
-     return json_encode($this->get_data_for_result());
- }
- public function  dependency_injection()
- {
-     $archive = new activity_data();
-     return $archive;
+    public $meeting_id;
+    public $sql_result;
+    public $data;
+    public function get_meeting_id(){
+        return $this->meeting_id;
+    }
+    public function get_sql_result()
+    {return $this->sql_result;}
+    public function get_data()
+    {return $this->data;}
+    public $database;
+    public function content()
+    {
+        $this->set_data();
+        \drupal::logger("nfb_washington_ajax")->notice(print_r($this->get_data(),true));
+        return new JsonResponse($this->get_data());
+    }
 
- }
- public function recover_rep_id()
- {
-     $request = Request::createFromGlobals();
-     $this->rep_id =  $request->request->get('rep_id');
- }
- public function get_meeting_details()
- {
-     $this->archive = $this->dependency_injection();
-     $sem_id = $this->get_rep_id();
-     $this->archive->find_rep_name($sem_id, $array);
-     $this->result_array = $array;
- }
- public function parse_result_array()
- {
-     if($this->get_results_array()['issue1'] != "no rating")
-     {$ratings_array['0'] = $this->get_results_array()['issue1'];}
-     else{$ratings_array['0'] = "no_value";}
-     if($this->get_results_array()['issue2'] != "no rating")
-     {$ratings_array['1'] = $this->get_results_array()['issue2'];}
-     else{$ratings_array['1'] = "no_value";}
-     if($this->get_results_array()['issue3'] != "no rating")
-     {$ratings_array['2'] = $this->get_results_array()['issue3'];}
-     else{$ratings_array['2'] = "no_value";}
-     $ratings_array['3'] = $this->get_results_array()['comment1'];
-     $ratings_array['4'] = $this->get_results_array()['comment2'];
-     $ratings_array['5'] = $this->get_results_array()['comment3'];
- }
- public function get_data_for_result()
- {
-     $this->recover_rep_id();
-     $this->get_meeting_details();
-     $this->parse_result_array();
-     return $this->get_ratings_array();
- }
+    public function request_js_data()
+    {
+        $request = Request::createFromGlobals();
+        $this->meeting_id = $request->request->get('meetingid');
+    }
+    public function set_data()
+    {
+        $this->request_js_data();
+        $this->find_ratings();
+        $this->loop();
 
+    }
+    public function find_ratings()
+    {
+        $this->database = new base();
+        $query = "select * from nfb_washington_rating where activity_id = '".$this->get_meeting_id()."'
+        order by issue_id ASC;";
+        $key = 'issue_id';
+        $this->database->select_query($query, $key);
+        $this->sql_result = $this->database->get_result();
+        $this->database = null;
+    }
+    public function loop()
+    {
+        $count = 1;
+        foreach ($this->get_sql_result() as $rating)
+        {
+            $rating = get_object_vars($rating);
+            switch ($count)
+            {
+                case 1:
+                    $this->set_issue_1($rating, $data);  break;
+                case 2:
+                    $this->set_issue_2($rating, $data);  break;
+                case 3:
+                    $this->set_issue_3($rating, $data);  break;
+            }
+            $count++;
+        }
+        $this->data = $data;
+    }
+    public function set_issue_1($rating, &$data)
+    {
+        $new_rating = $rating['rating'];
+        $this->convert_rating($new_rating);
+        $data[0] = $new_rating;
+        $data[1] = $rating['comment'];
+    }
+    public function set_issue_2($rating, &$data)
+    {
+        $new_rating = $rating['rating'];
+        $this->convert_rating($new_rating);
+        $data[2] = $new_rating;
+        $data[3] = $rating['comment'];
+    }
+    public function set_issue_3($rating, &$data)
+    {
+        $new_rating = $rating['rating'];
+        $this->convert_rating($new_rating);
+        $data[4] = $new_rating;
+        $data[5] = $rating['comment'];
+    }
+    public function convert_rating(&$new_rating)
+    {
+        switch($new_rating)
+        {
+            case 'y':
+                $rating = "Yes"; break;
+            case "n":
+                $rating = "No"; break;
+            case "u":
+                $rating = "Undecided"; break;
+            case "nd":
+                $rating = "Not Discussed"; break;
+        }
+        $new_rating = $rating;
+    }
 
 }
