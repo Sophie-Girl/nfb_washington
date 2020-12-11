76a07ae93312b4f25fb62df1a65486934858f6bc
sconnell <sconnell@nfb.org>
2020-12-11 17:38:12 -0500

    start committee issue form

--- src/form_factory/report/individual_member_report.php
+++ src/form_factory/report/individual_member_report.php
@@ -1,10 +1,13 @@
 <?php
 namespace  Drupal\nfb_washington\form_factory\report;
+use Drupal\civicrm\Civicrm;
+use Drupal\nfb_washington\civicrm\civi_query;
 use Drupal\nfb_washington\database\base;
 
 class individual_member_report
 {
     public $database;
+    public $civi_query;
     public $user_permission;
     public function get_user_permission()
     {return $this->user_permission;}
@@ -29,7 +32,21 @@ class individual_member_report
     public $propublica_id;
     public function get_propublica_id()
     {return $this->propublica_id;}
-
+    public $first_name;
+    public function get_first_name()
+    {return $this->first_name;}
+    public $last_name;
+    public function get_last_name()
+    {return $this->last_name;}
+    public $phone_number;
+    public function get_phone_number()
+    {return $this->phone_number;}
+    public $link_id;
+    public function get_link_id()
+    {return $this->link_id;}
+    public $note_markup;
+    public function get_note_markup()
+    {return $this->note_markup;}
     public function set_user_permission()
     {
         $user = \Drupal::currentUser(); $permission = "false";
@@ -69,6 +86,86 @@ class individual_member_report
             $this->propublica_id = $member['propublica_id'];
         }
     }
+    public function get_contact_info()
+    {
+        $civi = new Civicrm(); $civi->initialize();
+        $this->civi_query = new civi_query($civi);
+        $this->civi_query->civi_entity = "Contact";
+        $this->civi_query->civi_mode = 'get';
+        $this->civi_query->civi_params = array(
+            'sequential' => 1,
+            'id' => $this->get_civicrm_id(),
+        );
+        $this->civi_query->civi_query();
+        foreach($this->civi_query->get_civicrm_result() as $contact)
+        {
+            $this->first_name = $contact['first_name'];
+            $this->last_name =  $contact['last_name'];
+            $this->phone_number = $contact['phone'];
+        }
+        $this->civi_query = null;
+    }
+    public function get_notes()
+    {
+        $this->database = new base();
+        $query = "select   * from nfb_washington_note_link where entity_id = '".$this->get_member_id()."'
+        and table_name = 'nfb_washington_members';";
+        $key = "link_id";
+        $this->database->select_query($query, $key);
+        $this->sql_result = $this->database->get_result();
+        $this->database = null;
+    }
+    public function note_link_loop()
+    {
+        $note_markup = "<h3>Notes</h3>";
+        $links =  $this->get_sql_result();
+        $count = 0;
+        foreach($links as $link)
+        {
+            $link = get_object_vars($link);
+            $this->link_id = $link['link_id'];
+            $note_id = $link['note_id'];
+            $this->note_loop($note_markup, $note_id, $count);
+        }
+        if($count == 0)
+        {
+            $note_markup = "<h3>Notes</h3>
+        <p>No notes exist for this member</p>";
+            if($this->get_user_permission() == "true")
+            {
+                $note_markup = $note_markup."<p><a href='/nfb_washington/admin/notes/create' class='button-1' role ='button' aria-label='create new note'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Create New&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='/nfb_washington/admin/note_link' class='button-2' role='button' aria-label='Link Existing Note'>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Link Existing&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></p>";
+            }
+        }
+
+    }
+    public function note_loop(&$note_markup, $note_id, &$count)
+    {
+
+        $year = date('Y');
+        $this->database = new base();
+        $query = "select * from nfb_washington_note where note_id = '".$note_id."';";
+        $key = "note_id";
+        $this->database->select_query($query, $key);
+        foreach($this->get_sql_result() as $note)
+        { $note = get_object_vars($note);
+        if($note['note_year'] == $year)
+        {
+         $note_markup = $note_markup. "<p>- ".$note['note']."</p>";
+         if($this->get_user_permission() == "true")
+         {
+             $note_markup = $note_markup."<p><a href='/nfb_washington/admin/notes/create' class='button-1' role ='button' aria-label='create new note'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Create New&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href='/nfb_washington/admin/note_link' class='button-2' role='button' aria-label='Link Existing Note'>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Link Existing&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href='/nfb_washington/admin/note/remove.".$this->get_link_id()."' class='button-3' role='button' aria-label='Remove note'>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Remove This Note&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</a></p>";
+         }
+         $count++;
+        }}
+        $this->database = null;
+    }
+    public function relevant_committees_markup()
+    {
+
+    }
+
 
 
 }
