7f48da4f2b41ee273da1e1f9f6dc7dbcdea99605
sconnell <sconnell@nfb.org>
2020-12-17 13:08:04 -0500

    word doc creator, created.

--- src/microsoft_office/html_to_word.php
+++ src/microsoft_office/html_to_word.php
@@ -3,5 +3,20 @@ Namespace Drupal\nfb_washington\microsoft_office;
 class html_to_word
 {
 
+    public $font_size;
+    public function get_font_size()
+    {return $this->font_size;}
+    public $report_name;
+    public function get_report_name(){
+    return $this->report_name;}
+    public function download_doc($text)
+    {
+        $phpWord = new \PhpOffice\PhpWord\PhpWord();
+        $section = $phpWord->addSection();
+        $section->addText($text,
+            array('name' => 'Tahoma', 'size' => $this->get_font_size()));
+        $objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+        $objWriter->save($this->get_reportname().'.docx');
+    }
 
 }
