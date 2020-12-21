d9601af36f9ea3d523b52ac56f3ac9b3a556eba8
sconnell <sconnell@nfb.org>
2020-12-21 13:39:35 -0500

    word doc creator, created.

--- src/Form/IndMemberReportForm.php
+++ src/Form/IndMemberReportForm.php
@@ -3,10 +3,13 @@ Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\nfb_washington\form_factory\report\individual_member_report;
+use Drupal\nfb_washington\microsoft_office\html_to_word;
+use Drupal\nfb_washington\post_process\report\ind_member_donwlaod;
 
 class IndMemberReportForm extends FormBase
 {
     public $form_factory;
+    public $backend;
     public function getFormId()
     {
        return "wash_sem_ind_report";
@@ -28,6 +31,9 @@ class IndMemberReportForm extends FormBase
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
-        // TODO: Implement submitForm() method.
+        $form_factory = new individual_member_report();
+        $php_word = new html_to_word();
+        $this->backend = new ind_member_donwlaod($php_word, $form_factory);
+        $this->backend->backend($form_state);
     }
 }
