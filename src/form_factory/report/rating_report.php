d83067df887e3f26a73aa3cd5d46140d7309ffc0
sconnell <sconnell@nfb.org>
2020-12-29 02:57:24 -0500

    fixes for meeting download

--- src/form_factory/report/rating_report.php
+++ src/form_factory/report/rating_report.php
@@ -1,5 +1,6 @@
 <?php
 namespace  Drupal\nfb_washington\form_factory\report;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\nfb_washington\database\base;
 
 class rating_report extends meeting_report
@@ -18,6 +19,7 @@ class rating_report extends meeting_report
     public $issue_1_name;
     public $issue_2_name;
     public $issue_3_name;
+    public$file_type;
     public function get_rating_id()
     {return $this->rating_id;}
     public function get_issue_1_rating()
@@ -42,12 +44,55 @@ class rating_report extends meeting_report
     {return $this->issue_1_name;}
     public function get_issue_2_name()
     {return $this->issue_2_name;}
-    public function get_issue_3name()
+    public function get_issue_3_name()
     {return $this->issue_3_name;}
+    public function  get_file_type()
+    {return $this->file_type;}
     public function build_rating_form(&$form, $form_state)
     {
+        $form['intro_markup'] = array(
+            '#type' => 'item',
+            '#markup' => "<p>This report download will show  all meetings in order of the day in which they will happen in a word document</p>",
+        );
+        $form['file_type'] = array(
+          '#type' => 'select',
+          '#title' => "Select File Type",
+          '#options' => array('csv' => "CSV Excel File",
+              "docx" => "Word Document fro brailling"),
+            '#required' => true
+        );
+        $form['submit'] = array(
+            '#type' => 'submit',
+            '#value' => "Download",
+        );
 
-
+    }
+    public function backend_markups_and_array(FormStateInterface $form_state)
+    {
+        $this->file_type = $form_state->getValue("file_type");
+        $this->get_issues();
+        $this->full_member_query();
+        $ratings_array = [];
+        foreach ($this->get_member_results() as $member)
+        {
+            $member = get_object_vars($member);
+            $this->state = $member['state'];
+            $this->rank = $member['rank'];
+            $this->district = $member['district'];
+            $this->civi_id = $member['civicrm_contact_id'];
+            $this->civi_query_stuff();
+            $this->rating_issue_1_query();
+            $this->rating_issue_2_query();
+            $this->rating_issue_3_query();
+            $this->build_array($ratings_array);
+        }
+        if($this->get_file_type() == "csv")
+        {
+            // todo csv function
+        }
+        else {
+            // todo word markup download
+        }
     }
     public function rating_issue_1_query()
     {
@@ -55,13 +100,15 @@ class rating_report extends meeting_report
          $query = "select * from nfb_washington_rating where member_id = '".$this->get_member_id()."' and issue_id = '".$this->get_issue_1_id()."';";
          $key = 'rating_id';
          $this->database->select_query($query, $key);
-         foreach ($this->database->get_result() as $rating)
-         {
-             $rating = get_object_vars($rating);
-             $this->rating_switch($rating, $rating_value);
-             $this->issue_1_rating = $rating_value;
-             $this->issue_1_comment = $rating['comment'];
+         if($this->database->get_result() == array()) {
+             foreach ($this->database->get_result() as $rating) {
+                 $rating = get_object_vars($rating);
+                 $this->rating_switch($rating, $rating_value);
+                 $this->issue_1_rating = $rating_value;
+                 $this->issue_1_comment = $rating['comment'];
+             }
          }
+
     }
     public function rating_issue_2_query()
     {
@@ -69,26 +116,30 @@ class rating_report extends meeting_report
         $query = "select * from nfb_washington_rating where member_id = '".$this->get_member_id()."' and issue_id = '".$this->get_issue_2_id()."';";
         $key = 'rating_id';
         $this->database->select_query($query, $key);
-        foreach ($this->database->get_result() as $rating)
-        {
-            $rating = get_object_vars($rating);
-            $this->rating_switch($rating, $rating_value);
-            $this->issue_2_rating = $rating_value;
-            $this->issue_2_comment = $rating['comment'];
+        if($this->database->get_result() == array()) {
+            foreach ($this->database->get_result() as $rating) {
+                $rating = get_object_vars($rating);
+                $this->rating_switch($rating, $rating_value);
+                $this->issue_2_rating = $rating_value;
+                $this->issue_2_comment = $rating['comment'];
+            }
         }
+
     }
     public function rating_issue_3_query()
     {
         $this->database = new base();
-        $query = "select * from nfb_washington_rating where member_id = '".$this->get_member_id()."' and issue_id = '".$this->get_issue_1_id()."';";
+        $query = "select * from nfb_washington_rating where member_id = '".$this->get_issue_3_id()."' and issue_id = '".$this->get_issue_1_id()."';";
         $key = 'rating_id';
         $this->database->select_query($query, $key);
-        foreach ($this->database->get_result() as $rating)
-        {
-            $rating = get_object_vars($rating);
-            $this->rating_switch($rating, $rating_value);
-            $this->issue_1_rating = $rating_value;
-            $this->issue_1_comment = $rating['comment'];
+
+        if($this->database->get_result() == array()) {
+            foreach ($this->database->get_result() as $rating) {
+                $rating = get_object_vars($rating);
+                $this->rating_switch($rating, $rating_value);
+                $this->issue_1_rating = $rating_value;
+                $this->issue_1_comment = $rating['comment'];
+            }
         }
     }
     public function rating_switch($rating, &$rating_value)
@@ -134,6 +185,20 @@ class rating_report extends meeting_report
             $count++;
         }
         $this->database = null;
+    }
+    public function build_array(&$ratings_array)
+    {
+        $array_key = $this->get_state().$this->get_last_name().$this->get_first_name();
+        $ratings_array[$array_key]['first_name'] = $this->get_first_name();
+        $ratings_array[$array_key]['last_name'] = $this->get_last_name();
+        $ratings_array[$array_key]['phone'] = $this->get_phone();
+        $ratings_array[$array_key]['district_text'] = $this->district_text();
+        $ratings_array[$array_key][$this->get_issue_1_name()."_rating"] = $this->get_issue_1_rating();
+        $ratings_array[$array_key][$this->get_issue_1_name()."_comment"] = $this->get_issue_1_comment();
+        $ratings_array[$array_key][$this->get_issue_2_name()."_rating"] = $this->get_issue_2_rating();
+        $ratings_array[$array_key][$this->get_issue_2_name()."_comment"] = $this->get_issue_2_comment();
+        $ratings_array[$array_key][$this->get_issue_3_name()."_rating"] = $this->get_issue_3_rating();
+        $ratings_array[$array_key][$this->get_issue_3_name()."_comment"] = $this->get_issue_3_comment();
     }
 
 }
