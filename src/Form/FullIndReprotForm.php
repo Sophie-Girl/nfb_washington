ccae16ee75fa902d79d5173bc17021a42a018f6e
sconnell <sconnell@nfb.org>
2021-01-21 15:49:41 -0500

    Final version 2.2

---
+++ src/Form/FullIndReprotForm.php
@@ -0,0 +1,25 @@
+<?php
+Namespace Drupal\nfb_washington\Form;
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\nfb_washington\form_factory\report\all_ind_report;
+
+class FullIndReprotForm extends FormBase
+{
+    public $form_factory;
+    public function getFormId()
+    {
+        return "nfb_washington_all_mem_report";
+    }
+    public function buildForm(array $form, FormStateInterface $form_state)
+    {
+        $this->form_factory = new all_ind_report();
+        $this->buildForm($form, $form_state);
+        $this->form_factory = null;
+        return $form;
+    }
+    public function submitForm(array &$form, FormStateInterface $form_state)
+    {
+        // TODO: Implement submitForm() method.
+    }
+}
---
+++ src/form_factory/report/all_ind_report.php
@@ -0,0 +1,20 @@
+<?php
+namespace  Drupal\nfb_washington\form_factory\report;
+use Drupal\Core\Form\FormStateInterface;
+
+class all_ind_report
+{
+
+    public function  build_form_array(&$from, FormStateInterface $form_state)
+    {
+        $form['report_markup'] = array(
+            '#type' => 'item',
+            '#markup' => "Creates a large word doc with all members of congress for brailling"
+         );
+        $form['submit'] = array(
+            '#type' => 'submit',
+            '#value' => "Download",
+        );
+
+    }
+}
