457d671b5ae745f65fd8a8bf6a6bff63d1fcc9f8
sconnell <sconnell@nfb.org>
2021-01-25 14:17:15 -0500

    Final version 2.3

---
+++ src/Controller/AdminFAQCOntroller.php
@@ -0,0 +1,11 @@
+<?php
+Namespace Drupal\nfb_washington\Controller;
+use Drupal\Core\Controller\ControllerBase;
+class  AdminFAQCOntroller extends ControllerBase
+{
+    public function content()
+    {
+        $page = "<p>This text will be filled in at a later date</p>";
+        return $page;
+    }
+}
