3ebbc137c47eaa3d8dd85050d1c7119c63507ab1
sconnell <sconnell@nfb.org>
2021-01-26 09:56:15 -0500

    Final version 2.3

--- src/Controller/AdminFAQController.php
+++ src/Controller/AdminFAQController.php
@@ -5,7 +5,12 @@ class  AdminFAQController extends ControllerBase
 {
     public function content()
     {
-        $page = "<p>This text will be filled in at a later date</p>";
+        $page = "
+         <h2>Propublica API Information</h2>
+         <p>This system relies on the Propublica Congressional API. Here are some answers for basic questiosn on the data source</p>
+         <h3> How often does the data refresh?</h3>
+         <p> Data is refreshed daily, while vote details updated every 30 minutes.</p>
+         <h3> </h3>";
         return [
             '#type' => 'markup',
             '#markup' => $page,
