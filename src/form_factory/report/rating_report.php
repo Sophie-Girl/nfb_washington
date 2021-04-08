a75e2dd0d6c750329631862df5201a9fa0fbe682
sconnell <sconnell@nfb.org>
2021-04-08 11:13:11 -0400

    handle file stuff

--- src/form_factory/report/rating_report.php
+++ src/form_factory/report/rating_report.php
@@ -156,7 +156,7 @@ class rating_report extends meeting_report
             $text = $this->get_markup();
             $this->phpoffice = new html_to_word();
             $this->phpoffice->font_size = '12'; $year = date('Y');
-            $this->phpoffice->report_name = $year."_washington_seminar_rating_Report.docx";
+            $this->phpoffice->report_name = "/drupal/web/sites/nfb.org/files/".$year."_washington_seminar_rating_Report.docx";
             $this->phpoffice->download_doc($text);
 
         }
@@ -369,7 +369,7 @@ class rating_report extends meeting_report
     public function csv_functions()
     {
         $data = $this->get_member_results(); $year = date("Y");
-        $filename = $year."_washington_seminar_rating_report.csv";
+        $filename = "/drupal/web/sites/nfb.org/files/".$year."_washington_seminar_rating_report.csv";
         $this->set_csv_header($data);
         $this->set_headers($filename);
         $this->check_file_size($data, $filename, $file, $size);
--- src/post_process/report/ind_member_donwlaod.php
+++ src/post_process/report/ind_member_donwlaod.php
@@ -49,7 +49,7 @@ class  ind_member_donwlaod
     public function set_php_office_values()
     {
         $this->phpoffice->font_size = 18;
-        $this->phpoffice->report_name = "individual_member_of_congress_report.docx";
+        $this->phpoffice->report_name = "/drupal/web/sites/nfb.org/files/individual_member_of_congress_report.docx";
     }
     public function member_id_set(FormStateInterface $form_state){
         $this->form_factory->member_id = $form_state->getValue("member_value");
