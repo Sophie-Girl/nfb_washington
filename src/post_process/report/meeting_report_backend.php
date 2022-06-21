a974355537719f3a46fb9d01eb303a44fca39d7e
sconnell <sconnell@nfb.org>
2022-06-21 13:20:24 -0400

    testing for Moh

---
+++ src/post_process/report/meeting_report_backend.php
@@ -0,0 +1,175 @@
+<?php
+namespace  Drupal\nfb_washington\form_factory\report;
+use Drupal\civicrm\Civicrm;
+use Drupal\nfb_washington\civicrm\civicrm_v4;
+use Drupal\nfb_washington\database\base;
+class meeting_report_backend
+{
+    /* ################################################################
+     * # Connell, Sophia: Code Clean up cause I was rushed af on this #
+     * # First file to use CiviAPI version 4.                         #
+     * ################################################################  */
+    public $database;
+    public $civicrm;
+    public function __construct(civicrm_v4 $civicrm_v4)
+    {
+        $this->civicrm = $civicrm_v4;
+    }
+    public $member_results;
+    public function get_member_results()
+    {return $this->member_results;}
+    public $member_id;
+    public function get_member_id()
+    {return $this->member_id;}
+    public $civi_id;
+    public function get_civicrm_id()
+    {return $this->civi_id;}
+    public $district;
+    public function get_district()
+    {return $this->district;}
+    public $rank;
+    public function get_rank()
+    {return $this->rank;}
+    public $state;
+    public function get_state()
+    {return $this->state;}
+    public $first_name;
+    public function get_first_name()
+    {return $this->first_name;}
+    public $last_name;
+    public function get_last_name()
+    {return $this->last_name;}
+    public $phone;
+    public function get_phone()
+    {return $this->phone;}
+    public $location;
+    public function get_location()
+    {return $this->location;}
+    public $date;
+    public function get_date()
+    {return $this->date;}
+    public $time;
+    public function get_time()
+    {return $this->time;}
+    public $nfb_contact;
+    public function get_nfb_contact()
+    {return $this->nfb_contact;}
+    public $nfb_phone;
+    public function get_nfb_phone()
+    {return $this->nfb_phone;}
+    public $moc_contact;
+    public function get_moc_contact()
+    {return $this->moc_contact;}
+    public $moc_attendance;
+    public function get_moc_attendance()
+    {return $this->moc_attendance;}
+
+    public function docx_backend(){
+        $this->full_member_query();
+        $this->start_full_download_markup();
+    }
+    public function full_member_query()
+    {
+        $this->database = new base();
+        $query = "Select * from nfb_washington_members where active = 0 order by state ASC ;";
+        $key = 'member_id';
+        $this->database->select_query($query, $key);
+        $this->member_results = $this->database->get_result();
+        $this->database = null;
+    }
+    public function download_markup()
+    {
+
+        $this->markup = $this->get_markup(). $this->get_first_name()." ".$this->get_last_name().PHP_EOL.
+            $this->district_text(). " Phone number: ".$this->get_phone().PHP_EOL.
+            "Zoom Meeting ID: ".$this->get_location()." Meeting date: ".$this->get_date().PHP_EOL.
+            "Meeting Time: ". $this->get_time(). PHP_EOL.
+            "NFB Contact: ".$this->get_nfb_contact(). " Phone: ".$this->get_nfb_phone(). PHP_EOL.
+            "Attending Meeting: ".$this->get_moc_attendance(). " MOC Contact: ". $this->get_moc_contact().PHP_EOL.
+            "---------------------------------------------------------------------".PHP_EOL;
+
+    }
+    public function meeting_first_query()
+    {
+        $this->database = new base(); $year = date("Y");
+        $query = "select * from nfb_washington_activities where meeting_year = '".$year."' 
+        order by meeting_date ASC;";
+        $key = 'activity_id';
+        $this->database->select_query($query, $key);
+        $this->member_results = $this->database->get_result();
+        $this->database = null;
+
+    }
+    public function begin_new_download_markup()
+    {
+        $year = date("Y");
+        $this->markup = $year." Washington Seminar Meetings Report".PHP_EOL.
+            "------------------------------------------------------------------".PHP_EOL;
+        $this->meeting_first_query();
+        $this->process_meeting_query();
+    }
+    public function process_meeting_query()
+    {
+        foreach($this->get_member_results() as $meeting)
+        {
+            $meeting = get_object_vars($meeting);
+            $this->member_id = $meeting['member_id'];
+            $this->location = $meeting['location'];
+            $this->date = $meeting['meeting_date'];
+            $this->time = $meeting['meeting_time'];
+            $this->nfb_contact = $meeting['nfb_contact'];
+            $this->nfb_phone = $meeting['nfb_phone'];
+            $this->moc_contact = $meeting['m_o_c_contact'];
+            $this->moc_attendance = $meeting['moc_attendance'];
+            $this->member_query_meeting_report();
+        }
+    }
+    public function member_query_meeting_report()
+    {
+        $this->database = new base();
+        $query = "select * from nfb_washington_members where member_id = '".$this->get_member_id()."';";
+        $key = "member_id";
+        $this->database->select_query($query, $key);
+        foreach ($this->database->get_result() as $member)
+        {
+            $member = get_object_vars($member);
+            $this->state = $member['state'];
+            $this->rank = $member['rank'];
+            $this->district = $member['district'];
+            $this->civi_id = $member['civicrm_contact_id'];
+            $this->civi_query_stuff();
+            $this->download_markup();
+        }
+    }
+    public function civi_query_stuff()
+    {
+        $this->civicrm->civi_entity = "Contact";
+        $this->civicrm->civi_mode = 'get';
+        $this->civicrm->civi_params = [
+            'select' => [
+                '*',
+                'phone.*',
+            ],
+            'join' => [
+                ['Phone AS phone', FALSE, NULL, ['phone.contact_id', '=', 'id']],
+            ],
+            'where' => [
+                ['id', '=', $this->get_civicrm_id()],
+                ['phone.is_primary', '=', TRUE],
+                // So using primary is needed for just grabbing one contact
+            ],
+            'limit' => 25,
+        ];
+        $result = $this->civicrm->civi_query_v4();
+        foreach($result as $contact)
+        {
+            $this->first_name = $contact['first_name'];
+            $this->last_name =  $contact['last_name'];
+            $this->phone = $contact['phone.phone'];
+        }
+        $this->civicrm = null;
+    }
+
+
+
+}
