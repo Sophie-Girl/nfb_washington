89c099ce68a8c8a6f4791642dfc032ed8ebbb6ec
sconnell <sconnell@nfb.org>
2022-07-07 15:56:13 -0400

    Start of 2023 updates api v4 conversion

--- src/form_factory/report/meeting_report.php
+++ src/form_factory/report/meeting_report.php
@@ -83,12 +83,10 @@ class meeting_report
           "#title" => "Select State",
           '#options' => $this->state_options(),
           '#states' => array(
-              'visible' => array(
-                  'element_id' => 'filter_results',
-                  'value' => "state"),
-              'connector_1' => 'and',
-              'required' => array('element_id' => 'filter_result',
-                  'value' => "state"),
+              'visible' => [':input[name="filter_results"]' => ['value' => "state"],
+              'and',
+              'required' => [':input[name="filter_results"]' => ['value' => "state"],
+
 
           ),
         );
