88720c6eaeeb85633fd2f75c9a45453418030a13
sconnell <sconnell@nfb.org>
2022-07-12 16:56:32 -0400

    Start of 2023 updates report backend

--- src/post_process/admin/admin_set_meetings.php
+++ src/post_process/admin/admin_set_meetings.php
@@ -10,6 +10,26 @@ class admin_set_meetings {
      *# empty token meetings that will allow for easy reporting      #
       ################################################################ */
     public $database;
+    public $year;
+    public function get_year()
+    {return $this->year;}
+    public function set_year()
+    {
+        $this->year = date('Y');
+    }
+    public function create_dummy_meetings()
+    {
+        $this->set_year();
+
+    }
+    public function grab_all_active()
+    {
+        $query = "SELECT * FROM nfb_washington_members where nfb_washington_members.active != 1;";
+        $this->database = new base();
+        $key = 'member_id';
+        $this->database->select_query($query, $key);
+
+    }
 
 
 
