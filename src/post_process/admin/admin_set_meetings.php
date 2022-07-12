f88273cdab7cb1b2e5cc093ef2e6982373c47114
sconnell <sconnell@nfb.org>
2022-07-12 16:51:34 -0400

    Start of 2023 updates report backend

--- src/post_process/admin/admin_set_meetings.php
+++ src/post_process/admin/admin_set_meetings.php
@@ -1,1 +1,16 @@
 <?php
+Namespace Drupal\nfb_washington\post_process\admin;
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\nfb_washington\database\base;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+class admin_set_meetings {
+    /*################################################################
+     *# Connell, Sophia: Creating this script as a solution to the  #
+     *# new request for reports on empty meetings. This will gen    #
+     *# empty token meetings that will allow for easy reporting      #
+      ################################################################ */
+    public $database;
+
+
+
+}
