65ea7aa227732b7cc235183e29fb66e2b4f58f8a
sconnell <sconnell@nfb.org>
2022-07-19 16:06:33 -0400

    API v4 conversion

--- src/civicrm_drupal_link/drupal_member_civi_contact_link.php
+++ src/civicrm_drupal_link/drupal_member_civi_contact_link.php
@@ -534,6 +534,25 @@ class drupal_member_civi_contact_link
             $this->maintnence_database();
         }
     }
+    public function find_civi_record_v4()
+    {
+        $this->civi_query->civi_mode = "get";
+        $this->civi_query->civi_entity = "Contact";
+        $this->civi_query->civi_params = array(
+            'sequential' => 1,
+            'contact_sub_type' => "Congressional_Representative",
+            'first_name' => $this->propublica_query->get_member_first_name(),
+            'last_name' => $this->propublica_query->get_member_last_name(),
+        );
+        $this->civi_query->civi_query();
+        if($this->civi_query->get_civicrm_result()['count'] > 0)
+        {
+            $this->drupal_civicrm_id = $this->civi_query->get_civicrm_result()['values']['0']['contact_id'];
+            // $this->deactivate_record();
+            $this->deactivate_record_v4(); // new version for API updates
+            $this->maintnence_database();
+        }
+    }
     public function  maintnence_database()
     {
         $this->database = new base();
