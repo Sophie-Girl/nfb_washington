21998eeb248575324fdd2ce87fef607ce8cac3ef
sconnell <sconnell@nfb.org>
2022-07-20 14:20:38 -0400

    API v4 conversion

--- src/post_process/report/rating_report_backend.php
+++ src/post_process/report/rating_report_backend.php
@@ -7,4 +7,19 @@ use Drupal\nfb_washington\database\base;
 class rating_report_backend
 {
 
+
+    public function file_download($file, $size, $filename)
+    {
+        if (strlen($file) > 0) {
+            ob_start();  // buffer all but headers
+            ob_end_clean();  // headers get sent, erase all buffering and enable output
+            header("Content-type: application/csv; charset=UTF-8");
+            header("Content-length: " . $size);
+            header('Pragma: public');
+            header("Content-Description: PHP Generated Data");
+            header('Content-Disposition: attachment; filename="' . $filename . '"');
+            echo $file;
+            unlink($filename);
+            exit;}
+    }
 }
