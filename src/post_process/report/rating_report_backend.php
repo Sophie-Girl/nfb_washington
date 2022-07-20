a323ca74f0c8bf679b3e41aaa819497f5a1c4e8f
sconnell <sconnell@nfb.org>
2022-07-20 13:53:51 -0400

    API v4 conversion

---
+++ src/post_process/report/rating_report_backend.php
@@ -0,0 +1,10 @@
+<?php
+namespace  Drupal\nfb_washington\post_process\report;
+use Drupal\civicrm\Civicrm; // V3 will become deprecated.
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\nfb_washington\civicrm\civicrm_v4;
+use Drupal\nfb_washington\database\base;
+class rating_report_backend
+{
+
+}
