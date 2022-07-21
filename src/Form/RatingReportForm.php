51028739ff01f0e1117e9640d22a59e0cf71fe9b
sconnell <sconnell@nfb.org>
2022-07-21 12:43:49 -0400

    API v4 conversion

--- src/Form/RatingReportForm.php
+++ src/Form/RatingReportForm.php
@@ -3,7 +3,9 @@ Namespace Drupal\nfb_washington\Form;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\nfb_washington\form_factory\report\rating_report;
-
+use Drupal\nfb_washington\post_process\report\rating_report_backend;
+use Drupal\nfb_washington\civicrm\civicrm_v4;
+use Drupal\civicrm\Civicrm;
 class  RatingReportForm extends FormBase
 {
     public  $form_factory;
@@ -20,8 +22,12 @@ class  RatingReportForm extends FormBase
     }
     public function submitForm(array &$form, FormStateInterface $form_state)
     {
-        $this->form_factory = new rating_report();
+        $civicrm = new Civicrm(); $civicrm->initialize();
+        $civicrm_v4 = new civicrm_v4($civicrm);
+        $this->form_factory = new rating_report_backend($civicrm_v4);
         $this->form_factory->backend_markups_and_array($form_state);
+
+
 
     }
 }
